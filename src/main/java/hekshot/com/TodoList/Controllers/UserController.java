e78079f299c2bc2881367ac670df565f5be16e05
hekshot <amankumarkr111@gmail.com>
2023-11-27 14:59:22 +0530

    code skimming

--- src/main/java/hekshot/com/TodoList/Controllers/UserController.java
+++ src/main/java/hekshot/com/TodoList/Controllers/UserController.java
@@ -7,12 +7,10 @@ import hekshot.com.TodoList.Entity.Login;
 import hekshot.com.TodoList.Entity.TodoList;
 import hekshot.com.TodoList.Entity.Users;
 import hekshot.com.TodoList.Repository.TodoListRepository;
-import hekshot.com.TodoList.Services.CaptchaValidator;
 import hekshot.com.TodoList.Services.UsernameAlreadyExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 import hekshot.com.TodoList.Repository.UsersRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -38,9 +36,6 @@ public class UserController {
     private TodoListRepository todoListRepository;
 
     @Autowired
-    private CaptchaValidator validator;
-
-    @Autowired
     private CaptchaUtil captchaUtil;
 
     private void getCaptcha(Users user) {
@@ -61,28 +56,6 @@ public class UserController {
         }, 1, TimeUnit.MINUTES); // 5 minutes (adjust the delay as needed)
     }
 
-//    // Scheduled method to clean up expired captchas periodically
-//    @Scheduled(fixedDelay = 60000) // Run every 1 minute (adjust as needed)
-//    private void cleanupExpiredCaptchas() {
-//        long currentTime = System.currentTimeMillis();
-//
-//        hm.entrySet().removeIf(entry -> {
-//            long captchaCreationTime = entry.getValue().getCreationTime();
-//            long elapsedTime = currentTime - captchaCreationTime;
-//            return elapsedTime > TimeUnit.MINUTES.toMillis(5); // Remove captchas older than 5 minutes
-//        });
-//    }
-
-//    @GetMapping("/captcha/{randomId}")
-//    public CaptchaRes generateCaptcha(@PathVariable("randomId") int randomId){
-//        captcha = CaptchaUtil.createCaptcha(240,70);
-//        hm.put(randomId, captcha.getAnswer());
-//        scheduleCaptchaCleanup(randomId);
-//        for (Map.Entry<Integer, String> entry : hm.entrySet()) {
-//            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
-//        }
-//        return new CaptchaRes(CaptchaUtil.encodeCaptcha(captcha));
-//    }
 
     @GetMapping("/captcha/{randomId}")
     public CaptchaRes generateCaptcha(@PathVariable("randomId") int randomId) {
@@ -104,26 +77,6 @@ public class UserController {
         return usersRepository.findById(userId).orElseThrow(() -> new NoSuchElementException());
     }
 
-//    @PostMapping("/register")
-//    public Users addUser(@RequestBody Users users /*@RequestParam("g-recaptcha-response")String captcha*/) throws UsernameAlreadyExistsException {
-//        Users existingUser = usersRepository.findByUserName(users.getUserName());
-//        if (existingUser != null) {
-//            throw new UsernameAlreadyExistsException("Username already exists");
-//        }
-////        else if(validator.isValid(captcha)) {
-////            users.setUserName(users.getUserName());
-////            users.setUserPassword(users.getUserPassword());
-////            return usersRepository.save(users);
-////        }
-////        else {
-////            throw new CaptchaValidationException("CAPTCHA validation failed");
-////        }
-//        else{
-//            users.setUserName(users.getUserName());
-//            users.setUserPassword(passwordEncoder.encode(users.getUserPassword()));
-//            return usersRepository.save(users);
-//        }
-//    }
 
     @PostMapping("/register/{randomId}")
     public ResponseEntity<?> addUser(@PathVariable("randomId") int randomId,@RequestBody Users users ) throws UsernameAlreadyExistsException {
@@ -146,60 +99,6 @@ public class UserController {
         }
     }
 
-    /*@PostMapping("/login")
-    public Users loginUser(@RequestBody Login login *//*@RequestParam("g-recaptcha-response")*//**//*String captcha*//*) {
-        Users user = usersRepository.findByUserName(login.getUserName());
-        *//*if (validator.isValid(captcha)) {*//*
-            if (user != null && user.getUserPassword().equals(login.getUserPassword())) {
-                // Successful login
-                //return ResponseEntity.ok("Login successful");
-                return user;
-            } else {
-                //return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
-                throw new RuntimeException();
-            }
-        }
-        *//*else {
-            throw new CaptchaValidationException("CAPTCHA validation failed");
-        }*//*
-    *//*}*/
-
-
-//    @PostMapping("/login")
-//    public Users loginUser(@RequestBody Login login) {
-//        Users user = usersRepository.findByUserName(login.getUserName());
-//
-//        if (user != null && passwordEncoder.matches(login.getUserPassword(), user.getUserPassword())) {
-//            // Passwords match, successful login
-//            return user;
-//        } else {
-//            // Return an informative response to the client
-//            throw new RuntimeException();
-//        }
-//    }
-
-
-//    @PostMapping("/login/{randomId}")
-//    public ResponseEntity<?> loginUser(@PathVariable("randomId") int randomId,@RequestBody Login login) {
-//        Users user = usersRepository.findByUserName(login.getUserName());
-//
-//        String code = hm.get(randomId);
-//
-//        if (user != null && passwordEncoder.matches(login.getUserPassword(), user.getUserPassword())) {
-//            // Passwords match, check captcha
-//            //if (login.getCaptcha().equals(captcha.getAnswer())) {
-//            if (login.getCaptcha().equals(code)) {
-//                // Captcha is correct, successful login
-//                return ResponseEntity.ok(user);
-//            } else {
-//                // Captcha is incorrect
-//                return ResponseEntity.ok("Incorrect Captcha");
-//            }
-//        } else {
-//            // Invalid username or password
-//            return ResponseEntity.ok("Invalid username or password");
-//        }
-//    }
 
     @PostMapping("/login/{randomId}")
     public ResponseEntity<?> loginUser(@PathVariable("randomId") int randomId,@RequestBody Login login) {
